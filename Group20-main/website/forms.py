52006fb876c22311f0355adf216c24419f958b91
DStayoch <145609116+DStayoch@users.noreply.github.com>
2023-11-20 17:53:05 -0500

    Delete Group20-main directory

--- Group20-main/website/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email
-
-class FeedbackForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    feedback = TextAreaField('Feedback', validators=[DataRequired()])
-    submit = SubmitField('Submit')
